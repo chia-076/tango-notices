40422ef02d70d53930e013717368f98625b97e78
chia <chia@gmail.com>
2016-08-23 14:51:45 +0800

    [TR-3469] minor fixed on entry points

--- src/main/java/com/wmg/dsp/tango/notices/entrypoint/FactoryApplicationImpl.java
+++ src/main/java/com/wmg/dsp/tango/notices/entrypoint/FactoryApplicationImpl.java
@@ -3,8 +3,10 @@ package com.wmg.dsp.tango.notices.entrypoint;
 import com.wmg.dsp.platform.AppLoader;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ComponentScan("com.wmg.dsp.tango.notices")
+@ComponentScan(ServiceApplication.PACKAGE)
+@ImportResource(value = "classpath:beans.xml")
 public class FactoryApplicationImpl implements AppLoader {
 }
--- src/main/java/com/wmg/dsp/tango/notices/entrypoint/ServiceApplication.java
+++ src/main/java/com/wmg/dsp/tango/notices/entrypoint/ServiceApplication.java
@@ -1,35 +1,17 @@
 package com.wmg.dsp.tango.notices.entrypoint;
 
-import com.wmg.dsp.platform.rest.jersey.DefaultJerseyExceptionHandler;
-import com.wmg.dsp.platform.rest.jersey.UriParamExceptionHandler;
-import org.glassfish.jersey.server.ResourceConfig;
+import com.wmg.dsp.tango.jazz.commons.bootstrap.entrypoint.BaseServiceApplication;
 
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath(ServiceApplication.PATH)
-public class ServiceApplication extends ResourceConfig {
+public class ServiceApplication extends BaseServiceApplication {
 
     public static final String PATH = "/api/v1";
+    public static final String PACKAGE = "com.wmg.dsp.tango.notices";
 
-    public ServiceApplication() {
-        // Register exception all handlers from jersey2-module except BadRequestExceptionHandler ,
-        // since we override it with our ConstraintViolationExceptionMapper, which is more relevant for data services.
-        // TODO this should be moved to jersey2-module after the module have an ability to disable its default exception handlers
-        super(DefaultJerseyExceptionHandler.class, UriParamExceptionHandler.class);
-
-        // init about endpoint
-        packages("com.wmg.dsp.platform.rest.jersey.info");
-
-        // swagger resource & provider
-        packages("com.wmg.dsp.platform.swagger.resources");
-
-        // swagger ui
-        packages("com.wmg.dsp.platform.swaggerui");
-
-        // rest-client
-        packages("com.wmg.dsp.platform.rest");
-
-        // Register everything from service's package
-        packages("com.wmg.dsp.tango.notices");
+    @Override
+    public String getServicePackage() {
+        return PACKAGE;
     }
 }
